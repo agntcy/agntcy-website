9c8e968cec5e748fe3fe88ac2fa7fe0ad80374e5
Jessie Qi (jesqi) <jesqi@cisco.com>
2025-03-05 20:00:01 -0500

    feat: seo updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,6 @@ const roboto = Roboto({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Agntcy Website",
-  description: "Internet of Agents Collective",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -34,3 +29,39 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Agntcy Website",
+  description: "An open source collective for inter-agent collaboration.",
+  openGraph: {
+    title: "Agntcy Website",
+    description: "An open source collective for inter-agent collaboration.",
+    url: `https://agntcy.org`,
+    siteName: "Agntcy",
+    images: [
+      {
+        url: `https://agntcy.org/logo/preview-thumbnail-new.png`,
+        alt: "Agntcy",
+        type: "image/png",
+        width: 942,
+        height: 530,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Agntcy Website",
+    description: "An open source collective for inter-agent collaboration.",
+    creator: "@outshiftbycisco",
+    images: [
+      "https://agntcy.org/logo/preview-thumbnail-new.png",
+    ],
+  },
+  metadataBase: new URL("https://agntcy.org/"),
+  alternates: {
+    canonical: "/",
+  },
+  robots: { index: true, follow: true },
+};
