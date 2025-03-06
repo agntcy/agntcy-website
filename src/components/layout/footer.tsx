f0fe788701dd4c1da562773f7784f287fce9ede0
Jessie Qi (jesqi) <jesqi@cisco.com>
2025-03-06 10:28:15 -0500

    feat: add privacy policy

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Link from "next/link";
+import { Button } from "components/ui/button";
+
+const Footer = () => {
+  return (
+    <div className="absolute bottom-8 lg:bottom-2 mt-4 flex flex-col sm:mt-0 sm:justify-center md:flex-row md:space-x-5 w-full">
+      <div className="container mx-auto px-2 flex items-end justify-end lg:items-start lg:justify-start">
+        <Button
+          asChild
+          variant={"link"}
+          aria-label={"Privacy Policy"}
+          className="px-0 text-sm font-thin  text-gray-200 sm:text-center"
+        >
+          <Link
+            href="https://www.cisco.com/c/en/us/about/legal/privacy-full.html"
+            target={"_blank"}
+          >
+            Privacy Policy
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
