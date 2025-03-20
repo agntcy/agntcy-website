7eaf5c176c63367e68ea73c55673f50e84816964
Jessie Qi <108147973+jesqi@users.noreply.github.com>
2025-03-19 20:35:09 -0400

    feat: update footer (#22)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,7 +5,20 @@ import { Button } from "components/ui/button";
 const Footer = () => {
   return (
     <div className="absolute bottom-8 lg:bottom-2 mt-4 flex flex-col sm:mt-0 sm:justify-center md:flex-row md:space-x-5 w-full">
-      <div className="container mx-auto px-2 flex items-end justify-end lg:items-start lg:justify-start">
+      <div className="container mx-auto px-2 flex items-end justify-end lg:items-start lg:justify-start gap-3 md:gap-4">
+        <Button
+          asChild
+          variant={"link"}
+          aria-label={"Terms & Conditions"}
+          className="px-0 text-sm font-thin  text-gray-200 sm:text-center"
+        >
+          <Link
+            href="https://www.cisco.com/c/en/us/about/legal/terms-conditions.html"
+            target={"_blank"}
+          >
+            Terms & Conditions
+          </Link>
+        </Button>
         <Button
           asChild
           variant={"link"}
@@ -17,6 +30,19 @@ const Footer = () => {
             target={"_blank"}
           >
             Privacy Policy
+          </Link>
+        </Button>
+        <Button
+          asChild
+          variant={"link"}
+          aria-label={"Privacy Policy"}
+          className="px-0 text-sm font-thin  text-gray-200 sm:text-center"
+        >
+          <Link
+            href="https://www.cisco.com/c/en/us/about/legal/trademarks.html"
+            target={"_blank"}
+          >
+            Trademarks
           </Link>
         </Button>
       </div>
