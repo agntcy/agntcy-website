c0bc6cb8025351e06a35d5de87e0bef5f21a7b28
RajaprabuGaneshan <rajapgan@cisco.com>
2025-04-07 15:13:07 +0530

    added the supporter page and updated the navigations
    Signed-off-by: RajaprabuGaneshan <rajapgan@cisco.com>

---
+++ src/app/contactus/page.tsx
@@ -0,0 +1,9 @@
+import ContactUs from "../../components/supporters/contact_us";
+
+export default function Supporters() {
+  return (
+    <div className="items-center justify-center bg-[#00142B]">
+      <ContactUs />
+    </div>
+  );
+}
---
+++ src/app/resources/page.tsx
@@ -0,0 +1,9 @@
+import Hero from "../../components/homepage/hero";
+
+export default function Home() {
+  return (
+    <div className="min-h-[calc(100vh-100px)] flex items-center justify-center">
+      <Hero />
+    </div>
+  );
+}
