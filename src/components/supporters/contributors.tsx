228fcba461419d564de680aa280afc69a0579559
Jessie Qi (jesqi) <jesqi@cisco.com>
2025-04-11 09:31:52 -0400

    chore: update logos
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>

--- src/components/supporters/contributors.tsx
+++ src/components/supporters/contributors.tsx
@@ -1,46 +1,5 @@
-/* eslint-disable */
-
-"use client";
 import RenderLogos from "../reusables/render-logo";
-
-const logos = [
-  {
-    name: "Traceloop",
-    src: "/images/traceloop.png",
-    width: 188,
-    height: 28,
-  },
-  {
-    name: "MongoDB",
-    src: "/images/mongodb-logo.png",
-    width: 159,
-    height: 40,
-  },
-  {
-    name: "Weaviate",
-    src: "/images/weaviate_WH.png",
-    width: 178,
-    height: 26,
-  },
-  {
-    name: "llamaindex",
-    src: "/images/llamaindex.png",
-    width: 174,
-    height: 31,
-  },
-  {
-    name: "Glean",
-    src: "/images/Glean.svg",
-    width: 78,
-    height: 31,
-  },
-  {
-    name: "Agency",
-    src: "/images/agency_white.png",
-    width: 122,
-    height: 36,
-  },
-];
+import { logos } from "data/logos";
 
 const Members = () => {
   return (
@@ -51,17 +10,11 @@ const Members = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center lg:grid-cols-3 gap-3 pb-8 md:pb-3 px-4 md:px-4">
           {RenderLogos(
-            logos.slice(0, 6),
+            logos.filter((logo) => logo.type === "members"),
             "lg:self-center lg:flex lg:justify-center"
           )}
-          {RenderLogos(logos.slice(6, 11), "lg:hidden")}
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center lg:grid-cols-5 lg:px-20 gap-3 pb-8 md:py-3">
-          {RenderLogos(
-            logos.slice(6, 11),
-            "hidden lg:self-center lg:flex lg:justify-center"
-          )}
-        </div>
+        
       </div>
     </div>
   );
