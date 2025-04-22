78f8cd99854acde0c6c647d82b0792d0d23e78da
Jessie Qi (jesqi) <jesqi@cisco.com>
2025-04-22 09:18:20 -0400

    chore: update font-size
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>

--- src/components/homepage/quotes.tsx
+++ src/components/homepage/quotes.tsx
@@ -33,7 +33,7 @@ const Quotes = () => {
               key={index}
               className="flex items-center justify-center basis-[90%] lg:basis-[80%]"
             >
-              <Card className="py-6 md:py-8 px-2 md:px-4 lg:px-8 min-h-[670px] md:min-h-[550px] lg::min-h-[420px] flex items-center justify-center">
+              <Card className="py-6 md:py-8 px-2 lg:px-8 min-h-[580px] md:min-h-[550px] lg::min-h-[420px] flex items-center justify-center">
                 <CardContent className="grid grid-cols-1 md:grid-cols-3  gap-4 md:gap-6 lg:gap-8 pt-6 items-center">
                   {quote.img && (
                     <div
@@ -55,7 +55,7 @@ const Quotes = () => {
                     </div>
                   )}
                   <div className="col-span-1 md:col-span-2 flex flex-col justify-center gap-2">
-                    <p className="text-xs sm:text-base md:text-lg pb-5">{quote.blurb}</p>
+                    <p className="text-sm sm:text-base md:text-lg pb-5">{quote.blurb}</p>
                     <div className="">
                       <p className="md:hidden text-right text-orange uppercase font-extrabold text-sm">
                         {quote.authorName.split(",").map((part, index) => (
