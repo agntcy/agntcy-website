992b3b42c9c15aafbf5b59c2b352897f0bdac5db
Jessie Qi (jesqi) <jesqi@cisco.com>
2025-05-23 12:33:02 -0400

    chore: update homepage
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>

--- src/components/supporters/steering_committee.tsx
+++ src/components/supporters/steering_committee.tsx
@@ -11,7 +11,8 @@ const SteeringCommittee = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center lg:grid-cols-3 gap-3 pb-8 md:pb-3 px-4 md:px-4">
           {RenderLogos(
-            logos.filter((logo) => logo.type === "Steering Committee"),
+            logos
+              .filter((logo) => logo.type === "Steering Committee"),
             "lg:self-center lg:flex lg:justify-center"
           )}
         </div>
