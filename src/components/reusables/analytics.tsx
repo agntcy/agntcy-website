3aaf6aba7eb11fffe2b9e64f2b144d717f3f6a85
Jessie Qi (jesqi) <jesqi@cisco.com>
2025-06-04 17:43:05 -0400

    chore: update segment call
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>

--- src/components/reusables/analytics.tsx
+++ src/components/reusables/analytics.tsx
@@ -1,33 +1,21 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { analytics } from "lib/segment";
 
 export default function Analytics() {
-  const [oneTrustLoaded, setOneTrustLoaded] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
-    if (window && window.OnetrustActiveGroups) {
-      setOneTrustLoaded(true);
-    } else {
-      // If not, set up an event listener or polling
-      const checkOneTrustLoaded = () => {
-        if (window && window.OnetrustActiveGroups) {
-          setOneTrustLoaded(true);
-          window.removeEventListener('onetrustloaded', checkOneTrustLoaded);
-        }
-      };
-      window.addEventListener('onetrustloaded', checkOneTrustLoaded);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (window && window.OnetrustActiveGroups?.includes("2")) {
+    if (
+      typeof window !== "undefined" &&
+      (window.OnetrustActiveGroups == undefined ||
+        window.OnetrustActiveGroups?.includes("2"))
+    ) {
       analytics.page();
     }
-  }, [pathname, oneTrustLoaded]);
+  }, [pathname]);
 
   return null;
 }
