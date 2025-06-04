14f03c91e284922caa7e52dcd3320cdb26b52a7b
Jessie Qi (jesqi) <jesqi@cisco.com>
2025-06-04 15:24:47 -0400

    fix: one trust and segment issue
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>

--- src/components/reusables/analytics.tsx
+++ src/components/reusables/analytics.tsx
@@ -1,17 +1,33 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { analytics } from "lib/segment";
 
 export default function Analytics() {
+  const [oneTrustLoaded, setOneTrustLoaded] = useState(false);
   const pathname = usePathname();
+
+  useEffect(() => {
+    if (window && window.OnetrustActiveGroups) {
+      setOneTrustLoaded(true);
+    } else {
+      // If not, set up an event listener or polling
+      const checkOneTrustLoaded = () => {
+        if (window && window.OnetrustActiveGroups) {
+          setOneTrustLoaded(true);
+          window.removeEventListener('onetrustloaded', checkOneTrustLoaded);
+        }
+      };
+      window.addEventListener('onetrustloaded', checkOneTrustLoaded);
+    }
+  }, []);
 
   useEffect(() => {
     if (window && window.OnetrustActiveGroups?.includes("2")) {
       analytics.page();
     }
-  }, [pathname]);
+  }, [pathname, oneTrustLoaded]);
 
   return null;
 }
