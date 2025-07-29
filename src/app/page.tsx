152c41933024500371309a66b6c4d3f10410ad5a
Jessie Qi (jesqi) <jesqi@cisco.com>
2025-07-29 09:32:20 -0400

    chore: remove whitepaper
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Quotes from "components/homepage/quotes";
 import Hero from "components/homepage/hero";
-import { Downloadable } from "~/components/homepage/downloadable";
+// import { Downloadable } from "~/components/homepage/downloadable";
 
 export default function Home() {
   return (
     <div className="min-h-[calc(100vh-100px)] flex flex-col items-center justify-center bg-bg">
       <Hero />
-      <Downloadable />
+      {/* <Downloadable /> */}
       <Quotes />
       <div className="bg-gradient-to-t from-bg to-bg h-10 w-full"></div>
     </div>
