14bf50c0f640419e81fbdcdc989bacf74c1ec8be
Jessie Qi (jesqi) <jesqi@cisco.com>
2025-11-18 12:42:42 -0500

    chore: update url
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,25 +5,25 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://agntcy.com',
+            url: 'https://agntcy.org',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
         },
         {
-            url: 'https://agntcy.com/changelog',
+            url: 'https://agntcy.org/changelog',
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 0.8,
         },
         {
-            url: 'https://agntcy.com/resources',
+            url: 'https://agntcy.org/resources',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.9,
         },
         {
-            url: 'https://agntcy.com/supporters',
+            url: 'https://agntcy.org/supporters',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.8,
