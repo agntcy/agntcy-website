1d6b1034f8cf78b6af584664265a2a4e5f05442d
Jessie Qi <108147973+jesqi@users.noreply.github.com>
2025-11-21 09:05:54 -0500

    chore: update meta tags (#127)
    * chore: meta tags
    
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>
    
    * chore: add fetchWithRetry
    
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>
    
    * Revert "chore: add fetchWithRetry"
    
    This reverts commit dc32deb63a99c90bf82bf536321810b3868bc66c.
    
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>
    
    ---------
    
    Signed-off-by: Jessie Qi (jesqi) <jesqi@cisco.com>

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from "next";
 
-const cspHeader = `
-  default-src 'self';
-  connect-src 'self' https://api.github.com;
-  font-src 'self' data: https://*.gstatic.com;
-  frame-src 'self' https://players.brightcove.net https://www.youtube.com https://*.hsforms.com https://*.hsforms.net https://*.hubspot.net https://*.hubspot.com https://*.cisco.com http://*.hsforms.net;
-  frame-ancestors 'none';
-  block-all-mixed-content;
-  base-uri 'self';
-  style-src 'self' 'unsafe-inline' https://*.googleapis.com;
-  script-src 'self' 'unsafe-inline' 'unsafe-eval';
-  img-src 'self' blob: data: https://*.amazonaws.com https://*.youtube.com https://*.ytimg.com;
-`;
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -21,40 +8,6 @@ const nextConfig: NextConfig = {
         hostname: "**",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-      source: "/:path*",
-      headers: [
-        {
-        key: "X-Content-Type-Options",
-        value: "nosniff",
-        },
-        {
-        key: "X-Frame-Options",
-        value: "SAMEORIGIN",
-        },
-        {
-        key: "Referrer-Policy",
-        value: "origin-when-cross-origin",
-        },
-        {
-        key: "X-XSS-Protection",
-        value: "1; mode=block",
-        },
-        {
-        key: "Permissions-Policy",
-        value: "clipboard-read=*, clipboard-write=*",
-        },
-        {
-        key: "Content-Security-Policy",
-        value: cspHeader.replace(/\n/g, '')
-        },
-      ],
-      },
-
-    ];
   },
 };
 
