bd53397f6e3534f83e11e5ba7fd6d9a6ba139650
Aron Kerekes <arkereke@cisco.com>
2026-07-02 18:47:21 +0200

    chore: updating slack invite link (#188)
    Signed-off-by: Aron Kerekes <arkereke@cisco.com>

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,5 +1,5 @@
 export const slackInviteUrl =
-  "https://join.slack.com/t/agntcy/shared_invite/zt-3hb4p7bo0-5H2otGjxGt9OQ1g5jzK_GQ";
+  "https://join.slack.com/t/agntcy/shared_invite/zt-3xozr6nzq-i6LXv2P8l2kVW4_Prnny2w";
 
 export const outlineCtaButtonClassName =
   "group inline-flex cursor-pointer items-center gap-1.5 rounded-full border border-[#187adc] bg-[#00142b] px-4 py-2 text-xs text-[#e8e9ea] transition-all duration-200 hover:-translate-y-0.5 hover:border-[#3b91e6] hover:bg-[#187adc]/10 hover:shadow-[0px_8px_24px_rgba(24,122,220,0.35)] md:px-5 md:py-2.5 md:text-sm";
